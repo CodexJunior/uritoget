9c8c88a8470c89089ad2832c6a00ff6d3843c414
CodexJunior <37993391+CodexJunior@users.noreply.github.com>
2020-03-08 02:55:30 -0500

    Create uritoget.js

---
+++ uritoget.js
@@ -0,0 +1,13 @@
+function uritoget(url){
+	var get=[];
+    var matches = url.match(/.*\?(.*)/);
+	if(matches!==null ? matches[1] != "" : false){
+     var querys = matches[1].split("&");
+     querys.forEach(function(query){
+     var param = query.split("=");
+     if(param[0]!="")
+     get[decodeURIComponent(param[0])] = (param[1]!= undefined ? decodeURIComponent(param[1]) : "");
+     });
+    }
+	return get;
+}
