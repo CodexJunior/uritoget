a0066d91fae1f10f17ab79ea261223e86d8f3fe7
CodexJunior <37993391+CodexJunior@users.noreply.github.com>
2020-06-21 20:11:30 -0500

    Update uritoget.js

--- uritoget.js
+++ uritoget.js
@@ -1,13 +1,13 @@
 function uritoget(url){
-	var get=[];
+    var get=[];
     var matches = url.match(/.*\?(.*)/);
-	if(matches!==null ? matches[1] != "" : false){
+    if(matches!==null ? matches[1] != "" : false){
      var querys = matches[1].split("&");
      querys.forEach(function(query){
-     var param = query.split("=");
-     if(param[0]!="")
-     get[decodeURIComponent(param[0])] = (param[1]!= undefined ? decodeURIComponent(param[1]) : "");
+       var param = query.split("=");
+       if(param[0]!="")
+       get[decodeURIComponent(param[0])] = (param[1]!=undefined ? decodeURIComponent(param[1]) : "");
      });
     }
-	return get;
+    return get;
 }
